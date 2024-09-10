477f2af15466fe79911b21db1efcf537fa022317
petr.pogorelov <petr.pogorelov@vk.team>
2024-09-10 09:58:30 +0300

    tracks from OK

--- botify/botify/experiment.py
+++ botify/botify/experiment.py
@@ -62,15 +62,6 @@ class Experiments:
     """
 
     AA = Experiment("AA", Split.HALF_HALF)
-    STICKY_ARTIST = Experiment("STICKY_ARTIST", Split.FOUR_WAY)
-    TOP_POP = Experiment("TOP_POP", Split.FOUR_WAY)
-    USER_BASED = Experiment("USER_BASED", Split.HALF_HALF)
-    PERSONALIZED = Experiment("PERSONALIZED", Split.HALF_HALF)
-    DSSM = Experiment("DSSM", Split.HALF_HALF)
-    CONTEXTUAL = Experiment("CONTEXTUAL", Split.HALF_HALF)
-    GCF = Experiment("GCF", Split.HALF_HALF)
-    DIVERSITY = Experiment("DIVERSITY", Split.HALF_HALF)
-    ALL = Experiment("ALL", Split.SEVEN_WAY)
 
     def __init__(self):
-        self.experiments = [Experiments.ALL]
+        self.experiments = [Experiments.AA]
